2dc129d7d22dcffc240fb3a66d5976ea00830dad
breadmk <minvita9999@naver.com>
2021-03-19 20:55:44 +0900

    project Start

---
+++ src/test/java/kr/co/bread/BreadApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
