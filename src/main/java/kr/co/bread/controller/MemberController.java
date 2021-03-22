f4ebbc8ff22f810a8c19ad830925b379d7c19374
breadmk <minvita9999@naver.com>
2021-03-23 01:36:11 +0900

    member signup

---
+++ src/main/java/kr/co/bread/controller/MemberController.java
@@ -0,0 +1,14 @@
+package kr.co.bread.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping("/member/signup")
+	public String singup() {
+		
+		return "/member/signup";
+	}
+}
